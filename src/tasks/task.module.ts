bc7cf67619313b4e52510220073ac318c5c50302
lewis1190 <lewiskos@hotmail.co.uk>
2026-05-14 21:50:26 +0100

    Removed unauth testing endpoints. Added node versioning for Heroku

--- src/tasks/task.module.ts
+++ src/tasks/task.module.ts
@@ -3,11 +3,10 @@ import { TaskService } from './task.service';
 import { TaskController } from './task.controller';
 import { FirebaseModule } from '../firebase/firebase.module';
 import { FirebaseAuthGuard } from '../auth/firebase-auth.guard';
-import { TempTaskController } from 'src/seed_helpers/add_task';
 
 @Module({
   imports: [FirebaseModule],
   providers: [TaskService, FirebaseAuthGuard],
-  controllers: [TaskController, TempTaskController],
+  controllers: [TaskController],
 })
 export class TaskModule {}
